2caae8addab952b43687f6eb9c27d82772810778
Andrew Peck <anp@ucla.edu>
2016-01-30 00:56:53 -0800

    Change from 1536 to 768 bit encoder

--- source/truncate_clusters.v
+++ source/truncate_clusters.v
@@ -34,7 +34,8 @@
 // to run over 2 or 3 clock cycles, which adds an overall latency but still
 // allows the priority encoding to be done at 160MHz without imposing much of
 // any constraint on the priority encoding logic.
-//
+//----------------------------------------------------------------------------------------------------------------------
+
 //----------------------------------------------------------------------------------------------------------------------
 `timescale 1ns / 100 ps
 //synthesis attribute ALLCLOCKNETS of truncate_clusters is "240MHz"
@@ -44,28 +45,23 @@ module truncate_clusters (
   input           clock,
   input     global_reset,
 
-  input latch_in,
+  input       latch_in,
   input [3:0] latch_delay,
 
-  input [1535:0]  vpfs_in,
-
-  output [1535:0] vpfs_out
+  input  [767:0] vpfs_in,
+  output [767:0] vpfs_out
 );
 
-//----------------------------------------------------------------------------------------------------------------------
-// phase
+parameter MXSEGS  = 12;
+parameter SEGSIZE = 768/MXSEGS;
+
+// latch enable
 //----------------------------------------------------------------------------------------------------------------------
 
 //(* max_fanout = 100 *)
 reg latch_en=0;
 SRL16E u_latchdly (.CLK(clock),.CE(1'b1),.D(latch_in),.A0(latch_delay[0]),.A1(latch_delay[1]),.A2(latch_delay[2]),.A3(latch_delay[3]),.Q(latch_dly));
-always @(posedge clock) begin
-  latch_en <= (latch_dly);
-end
-
-
-parameter MXSEGS  = 32;
-parameter SEGSIZE = 1536/MXSEGS;
+always @(posedge clock) latch_en <= (latch_dly);
 
 wire [SEGSIZE-1:0] segment           [MXSEGS-1:0];
 wire [SEGSIZE-1:0] segment_copy      [MXSEGS-1:0];
@@ -77,7 +73,6 @@ genvar iseg;
 generate;
 for (iseg=0; iseg<MXSEGS; iseg=iseg+1) begin: segloop
   initial segment_ff      [iseg] = {SEGSIZE{1'b0}};
-
 
   // remap cluster inputs into Segments
   assign segment[iseg]        = {vpfs_in [(iseg+1)*SEGSIZE-1:iseg*SEGSIZE]};
@@ -111,6 +106,25 @@ endgenerate
 
 // segments are kept (untruncated) if any preceeding segment has clusters
 
+//251MHz
+//assign segment_keep [15]  =  segment_active[14] | segment_active[13] | segment_active[12] | segment_active[11] | segment_active[10] | segment_active[9] | segment_active[8] | segment_active[7] | segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+//assign segment_keep [14]  =  segment_active[13] | segment_active[12] | segment_active[11] | segment_active[10] | segment_active[9] | segment_active[8] | segment_active[7] | segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+//assign segment_keep [13]  =  segment_active[12] | segment_active[11] | segment_active[10] | segment_active[9] | segment_active[8] | segment_active[7] | segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+//assign segment_keep [12]  =  segment_active[11] | segment_active[10] | segment_active[9] | segment_active[8] | segment_active[7] | segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+//255.448
+assign segment_keep [11]  =  segment_active[10] | segment_active[9] | segment_active[8] | segment_active[7] | segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+assign segment_keep [10]  =  segment_active[9] | segment_active[8] | segment_active[7] | segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+assign segment_keep [9]   =  segment_active[8] | segment_active[7] | segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+assign segment_keep [8]   =  segment_active[7] | segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+//251.762
+assign segment_keep [7]   =  segment_active[6] | segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+assign segment_keep [6]   =  segment_active[5] | segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+assign segment_keep [5]   =  segment_active[4] | segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+assign segment_keep [4]   =  segment_active[3] | segment_active[2] | segment_active[1] | segment_active[0];
+assign segment_keep [3]   =  segment_active[2] | segment_active[1] | segment_active[0];
+assign segment_keep [2]   =  segment_active[1] | segment_active[0];
+assign segment_keep [1]   =  segment_active[0];
+assign segment_keep [0]   =  0;
 
 //----------------------------------------------------------------------------------------------------------------------
 // Synthesis Estimate: 80 MHz
@@ -124,6 +138,7 @@ endgenerate
 //
 // assign segment_keep [17] =  segment_active[16] | segment_active[15] | segment_active[14] | segment_active[13] | segment_active[12] | segment_active[11] | segment_keep[11];
 // assign segment_keep [16] =                       segment_active[15] | segment_active[14] | segment_active[13] | segment_active[12] | segment_active[11] | segment_keep[11];
+
 // assign segment_keep [15] =                                            segment_active[14] | segment_active[13] | segment_active[12] | segment_active[11] | segment_keep[11];
 // assign segment_keep [14] =                                                                 segment_active[13] | segment_active[12] | segment_active[11] | segment_keep[11];
 // assign segment_keep [13] =                                                                                      segment_active[12] | segment_active[11] | segment_keep[11];
@@ -185,45 +200,45 @@ assign segment_keep [0]  =  0;
 //                     16 segments 207.211 MHz
 //----------------------------------------------------------------------------------------------------------------------
 // failed in par :(
-assign segment_keep [31] =  segment_active[30] | segment_active[29] | segment_active[28] | segment_active[27] | segment_keep[27];
-assign segment_keep [30] =                       segment_active[29] | segment_active[28] | segment_active[27] | segment_keep[27];
-assign segment_keep [29] =                                            segment_active[28] | segment_active[27] | segment_keep[27];
-assign segment_keep [28] =                                                                 segment_active[27] | segment_keep[27];
+// assign segment_keep [31] =  segment_active[30] | segment_active[29] | segment_active[28] | segment_active[27] | segment_keep[27];
+// assign segment_keep [30] =                       segment_active[29] | segment_active[28] | segment_active[27] | segment_keep[27];
+// assign segment_keep [29] =                                            segment_active[28] | segment_active[27] | segment_keep[27];
+// assign segment_keep [28] =                                                                 segment_active[27] | segment_keep[27];
+//
+// assign segment_keep [27] =  segment_active[26] | segment_active[25] | segment_active[24] | segment_active[23] | segment_keep[23];
+// assign segment_keep [26] =                       segment_active[25] | segment_active[24] | segment_active[23] | segment_keep[23];
+// assign segment_keep [25] =                                            segment_active[24] | segment_active[23] | segment_keep[23];
+// assign segment_keep [24] =                                                                 segment_active[23] | segment_keep[23];
+//
+// assign segment_keep [23] =  segment_active[22] | segment_active[21] | segment_active[20] | segment_active[19] | segment_keep[19];
+// assign segment_keep [22] =                       segment_active[21] | segment_active[20] | segment_active[19] | segment_keep[19];
+// assign segment_keep [21] =                                            segment_active[20] | segment_active[19] | segment_keep[19];
+// assign segment_keep [20] =                                                                 segment_active[19] | segment_keep[19];
+//
+// assign segment_keep [19] =  segment_active[18] | segment_active[17] | segment_active[16] | segment_active[15] | segment_keep[15];
+// assign segment_keep [18] =                       segment_active[17] | segment_active[16] | segment_active[15] | segment_keep[15];
+// assign segment_keep [17] =                                            segment_active[16] | segment_active[15] | segment_keep[15];
+// assign segment_keep [16] =                                                                 segment_active[15] | segment_keep[15];
 
-assign segment_keep [27] =  segment_active[26] | segment_active[25] | segment_active[24] | segment_active[23] | segment_keep[23];
-assign segment_keep [26] =                       segment_active[25] | segment_active[24] | segment_active[23] | segment_keep[23];
-assign segment_keep [25] =                                            segment_active[24] | segment_active[23] | segment_keep[23];
-assign segment_keep [24] =                                                                 segment_active[23] | segment_keep[23];
-
-assign segment_keep [23] =  segment_active[22] | segment_active[21] | segment_active[20] | segment_active[19] | segment_keep[19];
-assign segment_keep [22] =                       segment_active[21] | segment_active[20] | segment_active[19] | segment_keep[19];
-assign segment_keep [21] =                                            segment_active[20] | segment_active[19] | segment_keep[19];
-assign segment_keep [20] =                                                                 segment_active[19] | segment_keep[19];
-
-assign segment_keep [19] =  segment_active[18] | segment_active[17] | segment_active[16] | segment_active[15] | segment_keep[15];
-assign segment_keep [18] =                       segment_active[17] | segment_active[16] | segment_active[15] | segment_keep[15];
-assign segment_keep [17] =                                            segment_active[16] | segment_active[15] | segment_keep[15];
-assign segment_keep [16] =                                                                 segment_active[15] | segment_keep[15];
-
-assign segment_keep [15] =  segment_active[14] | segment_active[13] | segment_active[12] | segment_active[11] | segment_keep[11];
-assign segment_keep [14] =                       segment_active[13] | segment_active[12] | segment_active[11] | segment_keep[11];
-assign segment_keep [13] =                                            segment_active[12] | segment_active[11] | segment_keep[11];
-assign segment_keep [12] =                                                                 segment_active[11] | segment_keep[11];
-
-assign segment_keep [11] =  segment_active[10] | segment_active[9]  | segment_active[8] | segment_active[7] | segment_keep[7];
-assign segment_keep [10] =                       segment_active[9]  | segment_active[8] | segment_active[7] | segment_keep[7];
-assign segment_keep [9]  =                                            segment_active[8] | segment_active[7] | segment_keep[7];
-assign segment_keep [8]  =                                                                segment_active[7] | segment_keep[7];
-
-assign segment_keep [7]  =  segment_active[6]  | segment_active[5]  | segment_active[4] | segment_active[3] | segment_keep[3];
-assign segment_keep [6]  =  segment_active[5]  |                      segment_active[4] | segment_active[3] | segment_keep[3];
-assign segment_keep [5]  =  segment_active[4]                                           | segment_active[3] | segment_keep[3];
-assign segment_keep [4]  =                                                                segment_active[3] | segment_keep[3];
-
-assign segment_keep [3]  =  segment_active[2]  | segment_active[1]  | segment_active[0];
-assign segment_keep [2]  =  segment_active[1]  | segment_active[0];
-assign segment_keep [1]  =  segment_active[0];
-assign segment_keep [0]  =  0;
+//   assign segment_keep [15] =  segment_active[14] | segment_active[13] | segment_active[12] | segment_active[11] | segment_keep[11];
+//   assign segment_keep [14] =                       segment_active[13] | segment_active[12] | segment_active[11] | segment_keep[11];
+//   assign segment_keep [13] =                                            segment_active[12] | segment_active[11] | segment_keep[11];
+//   assign segment_keep [12] =                                                                 segment_active[11] | segment_keep[11];
+//
+//   assign segment_keep [11] =  segment_active[10] | segment_active[9]  | segment_active[8] | segment_active[7] | segment_keep[7];
+//   assign segment_keep [10] =                       segment_active[9]  | segment_active[8] | segment_active[7] | segment_keep[7];
+//   assign segment_keep [9]  =                                            segment_active[8] | segment_active[7] | segment_keep[7];
+//   assign segment_keep [8]  =                                                                segment_active[7] | segment_keep[7];
+//
+//   assign segment_keep [7]  =  segment_active[6]  | segment_active[5]  | segment_active[4] | segment_active[3] | segment_keep[3];
+//   assign segment_keep [6]  =  segment_active[5]  |                      segment_active[4] | segment_active[3] | segment_keep[3];
+//   assign segment_keep [5]  =  segment_active[4]                                           | segment_active[3] | segment_keep[3];
+//   assign segment_keep [4]  =                                                                segment_active[3] | segment_keep[3];
+//
+//   assign segment_keep [3]  =  segment_active[2]  | segment_active[1]  | segment_active[0];
+//   assign segment_keep [2]  =  segment_active[1]  | segment_active[0];
+//   assign segment_keep [1]  =  segment_active[0];
+//   assign segment_keep [0]  =  0;
 
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -262,12 +277,13 @@ assign segment_keep [0]  =  0;
 // assign segment_keep [1]  =  segment_active[0];
 // assign segment_keep [0]  =  0;
 
-assign vpfs_out = {
-                    segment_ff[31], segment_ff[30], segment_ff[29], segment_ff[28],
-                    segment_ff[27], segment_ff[26], segment_ff[25], segment_ff[24],
-                    segment_ff[23], segment_ff[22], segment_ff[21], segment_ff[20],
-                    segment_ff[19], segment_ff[18], segment_ff[17], segment_ff[16],
-                    segment_ff[15], segment_ff[14], segment_ff[13], segment_ff[12],
+
+assign vpfs_out = { //latch_en ? vpfs_in :  { // muxing vpfs_in to the output decreases overall processing latency by 1 clock cycle
+//                    segment_ff[31], segment_ff[30], segment_ff[29], segment_ff[28],
+//                    segment_ff[27], segment_ff[26], segment_ff[25], segment_ff[24],
+//                    segment_ff[23], segment_ff[22], segment_ff[21], segment_ff[20],
+//                    segment_ff[19], segment_ff[18], segment_ff[17], segment_ff[16],
+//                  segment_ff[15], segment_ff[14], segment_ff[13], segment_ff[12],
                     segment_ff[11], segment_ff[10], segment_ff[9],  segment_ff[8],
                     segment_ff[7],  segment_ff[6],  segment_ff[5],  segment_ff[4],
                     segment_ff[3],  segment_ff[2],  segment_ff[1],  segment_ff[0]};
